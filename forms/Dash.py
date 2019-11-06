07d626c58800be478217bf65b674a18add8529fc
bridget@anvil.works <bridget@anvil.works>
2019-11-06 16:17:16 +0000

    Saved automatically

--- forms/Dash.py
+++ forms/Dash.py
@@ -1,9 +1,8 @@
 from anvil import *
 
-class DashStats(DashStatsTemplate):
+class Dash(DashTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    
--- forms/Form1.py
+++ forms/Form1.py
@@ -1,9 +1,9 @@
 from anvil import *
 
-class DashStats(DashStatsTemplate):
+class Form1(Form1Template):
+
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    
