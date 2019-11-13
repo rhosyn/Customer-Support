d1ce434dbea5baf154ca0854a0cecdacd0c3ae98
bridget@anvil.works <bridget@anvil.works>
2019-11-13 11:18:40 +0000

    Saved automatically

--- forms/Tickets.py
+++ forms/Tickets.py
@@ -6,6 +6,9 @@ class Tickets(TicketsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.repeating_panel_1.items = [
-      {'name': 'Usman A', 'title': 'How do I update my DNS records?', 'category': 'Technical Support', 'agent':'Meredydd L', 'priority': 'Urgent', 'date': 'Just now'}
-    ]
+    row = {'name': 'Usman A', 'title': 'How do I update my DNS records?', 'category': 'Technical Support', 'agent':'Meredydd L', 'priority': 'Urgent', 'date': 'Just now'}
+
+    items = []
+    for i in range(20):
+      items.append(row)
+    self.repeating_panel_1.items = items
