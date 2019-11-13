5bf8459c89420c3729ffbf5a0dcbe67b4fbeb8b9
bridget@anvil.works <bridget@anvil.works>
2019-11-13 16:04:38 +0000

    Saved automatically

--- forms/Homepage.py
+++ forms/Homepage.py
@@ -1,5 +1,7 @@
 from anvil import *
 import plotly.graph_objs as go
+from .TicketsForm import TicketsForm
+from Dashboard import Dashboard
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
@@ -7,3 +9,18 @@ class Homepage(HomepageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+#     self.content_panel.add_component(TicketsForm(), full_width_row=True)
+#     self.ticket_link.role = 'active'
+
+  def link_2_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(Dashboard(), full_width_row=True)
+    self.dash_link.role = 'active'
+    
+    
+  def link_3_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(TicketsForm(), full_width_row=True)
+    self.ticket_link.role = 'active'
+
+
