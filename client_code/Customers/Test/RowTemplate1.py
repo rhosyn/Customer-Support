cdfbc7ddea42aff929c21904d873b989f38cfbb6
bridget@anvil.works <bridget@anvil.works>
2019-12-02 15:47:52 +0000

    broken cols

---
+++ client_code/Customers/Test/RowTemplate1.py
@@ -0,0 +1,11 @@
+from ._anvil_designer import RowTemplate1Template
+from anvil import *
+import anvil.server
+
+class RowTemplate1(RowTemplate1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+    self.repeating_panel_1.items = self.item['people']
