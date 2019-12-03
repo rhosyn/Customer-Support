84b3f5bdefb93100728ee8c53df25dc31f46e9bf
bridget@anvil.works <bridget@anvil.works>
2019-12-03 13:27:23 +0000

    Saved automatically

---
+++ client_code/Avatars/Module1.py
@@ -0,0 +1,11 @@
+import anvil.server
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example:
+#
+#    import Avatars.Module1
+#
+#    Avatars.Module1.say_hello()
+#
+
+def say_hello():
+  print "Hello, world"
