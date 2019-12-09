2803732e2ff89b0093da4b8e61e64ed15b4c25bb
bridget@anvil.works <bridget@anvil.works>
2019-12-09 12:15:01 +0000

    overlay height issue

--- client_code/Customers/CustomerOverviewContent/CustomerOverviewRP/__init__.py
+++ client_code/Customers/CustomerOverviewContent/CustomerOverviewRP/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import CustomerOverviewRPTemplate
 from anvil import *
 import anvil.server
+from ...CustomerOverlay import CustomerOverlay
 
 class CustomerOverviewRP(CustomerOverviewRPTemplate):
   def __init__(self, **properties):
@@ -8,3 +9,8 @@ class CustomerOverviewRP(CustomerOverviewRPTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def check_box_1_change(self, **event_args):
+    homepage = get_open_form()
+    homepage.add_component(CustomerOverlay(), slot="default")
+
