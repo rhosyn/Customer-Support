521f6fea16d5549c991433c62f755e91217f063f
bridget@anvil.works <bridget@anvil.works>
2019-12-18 14:32:26 +0000

    Saved automatically

--- client_code/Customers/CustomerOverviewSlots/CustomerOverview/__init__.py
+++ client_code/Customers/CustomerOverviewSlots/CustomerOverview/__init__.py
@@ -15,40 +15,40 @@ class CustomerOverview(CustomerOverviewTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    people = [{'name': 'Daniel', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-              {'name': 'Daniel Bercow', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-              {'name': 'Daniel Bob', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-              {'name': 'Daniel Barnes', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-              {'name': 'Daniel Bercow', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-              {'name': 'Daniel Bob', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'}
-             ]
-    people_c = [{'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-                {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-                {'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-                {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-                {'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
-                {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'}
-               ]
+#     people = [{'name': 'Daniel', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#               {'name': 'Daniel Bercow', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#               {'name': 'Daniel Bob', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#               {'name': 'Daniel Barnes', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#               {'name': 'Daniel Bercow', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#               {'name': 'Daniel Bob', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'}
+#              ]
+#     people_c = [{'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#                 {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#                 {'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#                 {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#                 {'name': 'Daniel Charles', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'},
+#                 {'name': 'Daniel Coz', 'last_name':'Barnes', 'company': 'Forty8Creates', 'telephone': '+44(0)20 710 13348', 'email':'daniel@forty8creates.com'}
+#                ]
     
     
     
-    data = [{'letter':'B', 'people': people},{'letter':'C', 'people': people_c}]
+#     data = [{'letter':'B', 'people': people},{'letter':'C', 'people': people_c}]
     
-    self.repeating_panel_1.items = data
-#     letters = list(string.ascii_uppercase)
-#     customers = anvil.server.call('get_customers')
-#     data = []
-#     for let in letters:
-#       people = [x for x in customers if x['last_name'][0].upper() == let]
-#       if people:
-#         data.append({'letter': let, 'people':people})
 #     self.repeating_panel_1.items = data
+    letters = list(string.ascii_uppercase)
+    customers = anvil.server.call('get_customers')
+    data = []
+    for let in letters:
+      people = [x for x in customers if x['last_name'][0].upper() == let]
+      if people:
+        data.append({'letter': let, 'people':people})
+    self.repeating_panel_1.items = data
 
 
     
 
-#   def select_all_box_change(self, **event_args):
-#     """This method is called when this checkbox is checked or unchecked"""
-#     pass
+  def select_all_box_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    pass
 
 
